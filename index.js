e323c1d3a427eb7d007d822f81cd0a407d16f825
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-24 18:53:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 // Write your code in this file!
 function scuberGreetingForFeet (distance) {
-  distance <= 400 ? return "This one is on me!":
-  distance > 2500 ? return "No can do.":
-  distance > 2000 ? return "I will gladly take your thirty bucks.":
+  if (distance <= 400 ) {return "This one is on me!" } 
+  else if (distance > 2500) {return "No can do."}
+  else if (distance > 2000) {return "I will gladly take your thirty bucks."}
 }
 
 function ternaryCheckCity(city){
-  if (city === "NYC") {return "Ok, sounds good."}
-  else {return "No go."}
-}
+  city === "NYC" ? return "Ok, sounds good."; return "No go."}
 
 function switchOnCharmFromTip(tip){
   if (tip === "generous"){return "Thank you so much."}
   else if (tip ==="not as generous"){return "Thank you."}
-  else {return "Bye."}
+  else {return ""}
 }
